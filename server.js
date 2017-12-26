eac7323781c60af23835619af3b75db44c7df405
xwa130 <xwa130@sfu.ca>
2017-12-25 23:35:06 -0800

    testtest

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 
 app.use(express.static('assets'));
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
-
+console.log('api', process.env.API_KEY);
 app.listen(process.env.PORT || 3000, () => console.log(`App is listening on port ${process.env.port || 3000}!`));
