90760c7a87dcf6affaee1e7071b840e974d1e142
xwa130 <xwa130@sfu.ca>
2017-12-25 16:50:58 -0800

    use dotenv to store environment variables

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
+require('dotenv').config();
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 import * as VueGoogleMaps from 'vue2-google-maps';
 import router from './router';
 import store from './store';
 import App from './pages/App';
-import { API_KEY } from './.env';
 import './node_modules/vuetify/dist/vuetify.min.css';
 
 Vue.use(Vuetify);
 Vue.use(VueGoogleMaps, {
   load: {
-    key: process.env.API_KEY || API_KEY,
+    key: process.env.API_KEY,
     libraries: 'places'
   }
 });
