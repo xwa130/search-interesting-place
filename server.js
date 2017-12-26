49994a91ee609b161513a7539934ec00d66809ab
xwa130 <xwa130@sfu.ca>
2017-12-26 11:41:42 -0800

    back api

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static('assets'));
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
-console.log('api', process.env.API_KEY);
-app.listen(process.env.PORT || 3000, () => console.log(`App is listening on port ${process.env.port || 3000}!`));
+console.log(process.env.API_KEY);
+app.listen(process.env.PORT || 3000, () => console.log(`App is listening on port ${process.env.PORT || 3000}!`));
